05ca2e8b03ee8d3b2d1f0cd0455c2c7dceb72710
jeongsol <jeongsol@outlook.com>
2024-01-25 15:28:29 +0900

    model predict test

--- ppp.py
+++ ppp.py
@@ -1,3 +1,4 @@
 from ultralytics import YOLO
 model = YOLO('./yolov8n.pt')
-results = model.predict(source="./test/", save = True)
+results = model.predict(source="./images/", save = True)
+print(results)

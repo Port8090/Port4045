baa2fc83dc42aa44836878f68176e969812cc8eb
shong <bbundnam@gmail.com>
2024-01-25 10:04:39 +0900

    shong

--- port4045/imgprocess/models.py
+++ port4045/imgprocess/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class MyModel(models.Model):
     my_file = models.FileField(upload_to='uploads/')
+
+""" class Post(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    image = models.ImageField(upload_to='images/',blank=True, null=True)
+    
+    def __str__(self):
+        return self.title """
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -12,3 +22,7 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
--- port4045/imgprocess/views.py
+++ port4045/imgprocess/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from PIL import Image , ImageFilter
 # Create your views here.
-from .models import MyModel
-
+from .models import MyModel, Post, Photo
+from django.utils import timezone
+from Flask import redirect
 def upload_file(request):
     if request.method == 'POST':
         my_file = request.FILES['file']
@@ -27,3 +28,25 @@ def process_image(image_path):
     print(f"Original Image Size: {image.size}")
     print(f"Resized Image Size: {resized_image.size}")
     print(f"Processed Image Size: {filtered_image.size}")
+
+def create(request):
+    if(request.method == 'POST'):
+        post = Post()
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.pub_date = timezone.datetime.now()
+        post.user = request.user
+        post.save()
+        # name 속성이 imgs인 input 태그로부터 받은 파일들을 반복문을 통해 하나씩 가져온다 
+        for img in request.FILES.getlist('imgs'):
+            # Photo 객체를 하나 생성한다.
+            photo = Photo()
+            # 외래키로 현재 생성한 Post의 기본키를 참조한다.
+            photo.post = post
+            # imgs로부터 가져온 이미지 파일 하나를 저장한다.
+            photo.image = img
+            # 데이터베이스에 저장
+            photo.save()
+        return redirect('/detail/' + str(post.id))
+    else:
+        return render(request, 'new.html')

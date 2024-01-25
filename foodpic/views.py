f38d14bac7f408165c8703bfe3753556a52de589
jeongsol <jeongsol@outlook.com>
2024-01-25 12:44:18 +0900

    업로드하면 파일 저장되고 디비에 추가됨

---
+++ foodpic/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import redirect, render
+from .forms import imageForm
+# Create your views here.
+
+
+
+def index(request):
+    form = imageForm()
+    return render(request, 'index.html', {'form': form})
+
+def create(request):
+    if request.method == 'POST':
+        form = imageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('foodpic')
+    else:
+        form = imageForm()
+    return redirect('foodpic')

a773536e965fd8607560d1941dbd94a4efdcdaf3
byepingu <3149nelle@gmail.com>
2024-01-25 17:58:32 +0900

    result.html add

--- foodpic/views.py
+++ foodpic/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .forms import imageForm
+from django.http import HttpResponse
+
+# from . import models
+# from .models import Question
+
 # Create your views here.
-
-
 
 def index(request):
     form = imageForm()
@@ -13,7 +16,9 @@ def create(request):
         form = imageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('foodpic:index')
+            
+            context = {'image_url': 'images/image3863.jpg'}
+            return render(request, 'result.html', context)
     else:
         form = imageForm()
     return redirect('foodpic')

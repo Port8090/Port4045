c28d2d1f73782b1753ab0533bf875c577b3e11fc
서다슬 <star.dss999@gmail.com>
2024-03-25 13:36:09 +0900

    Delete ppp.py

--- ppp.py
+++
@@ -1,4 +0,0 @@
-def predict_image():
-    from ultralytics import YOLO
-    model = YOLO('./yolov8n.pt')
-    r = model.predict(source=".\images\KakaoTalk_20231017_092510262_0rNq6QF.jpg", save = True)
